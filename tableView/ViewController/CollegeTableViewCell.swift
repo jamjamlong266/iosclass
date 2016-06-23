8431625c020e2db564aee9b163e976f0c74653fd
Jam Long <jam_long266@live.com>
2016-06-23 09:50:16 +0800

    upload URL

---
+++ tableView/ViewController/CollegeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollegeTableViewCell.swift
+//  tableView
+//
+//  Created by JaminPie on 23/06/2016.
+//  Copyright © 2016 jaminpie. All rights reserved.
+//
+
+import UIKit
+
+class CollegeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
